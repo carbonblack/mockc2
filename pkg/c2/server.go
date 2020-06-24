93acc368a01f162c7e2c8f8234cc6d227edec860
Scott Knight <4534275+knightsc@users.noreply.github.com>
2020-06-24 11:22:19 -0400

    Create central mock server
    Rather than each protocol having to run all the server
    code themselves the c2.Server type will start up a server
    on a specific port with a given protocol handler

---
+++ pkg/c2/server.go
@@ -0,0 +1,73 @@
+package c2
+
+import (
+	"fmt"
+	"net"
+	"sync"
+
+	"megaman.genesis.local/sknight/mockc2/internal/log"
+	"megaman.genesis.local/sknight/mockc2/pkg/protocol"
+)
+
+// A Server represents a running mock C2 server.
+type Server struct {
+	listener        net.Listener
+	quit            chan interface{}
+	wg              sync.WaitGroup
+	port            uint16
+	protocolHandler protocol.Handler
+}
+
+// NewServer creates a new mock C2 server and starts it listening on the
+// provided port.
+func NewServer(port uint16, protocolHandler protocol.Handler) (*Server, error) {
+	s := &Server{
+		quit:            make(chan interface{}),
+		port:            port,
+		protocolHandler: protocolHandler,
+	}
+
+	address := fmt.Sprintf(":%d", s.port)
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	s.listener = l
+	log.Debug("Server listening")
+
+	s.wg.Add(1)
+	go s.serve()
+
+	return s, nil
+}
+
+// Shutdown gracefully shuts down the C2 server without interrupting any
+// active connections.
+func (s *Server) Shutdown() {
+	close(s.quit)
+	s.listener.Close()
+	s.wg.Wait()
+}
+
+func (s *Server) serve() {
+	defer s.wg.Done()
+
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			select {
+			case <-s.quit:
+				return
+			default:
+				log.Warn("accept error %v", err)
+			}
+		} else {
+			s.wg.Add(1)
+			go func() {
+				//s.handleConnection(conn)
+				s.protocolHandler.HandleConnection(conn, s.quit)
+				s.wg.Done()
+			}()
+		}
+	}
+}
