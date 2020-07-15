31e185ac5209ace4f6a44e30611d486d14f02a54
Scott Knight <4534275+knightsc@users.noreply.github.com>
2020-07-15 15:53:46 -0400

    Add support for exec command

---
+++ internal/cli/exec.go
@@ -0,0 +1,16 @@
+package cli
+
+import (
+	"megaman.genesis.local/sknight/mockc2/pkg/c2"
+)
+
+func execCommand(agentID string, cmd []string) {
+	a := c2.AgentByID(agentID)
+
+	command := c2.ExecuteCommand{
+		Name: cmd[1],
+		Args: cmd[2:],
+	}
+
+	a.SendCommand(command)
+}
